e435546d5782eaf926882c26f803c8adb6a58b4d
joji espinoza <joji.espinoza@gmail.com>
2022-02-09 12:01:42 +0800

    add travel record ui and corresponding functionalities

---
+++ AdminPortal/AdminPortal/Controllers/TravelRecordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminPortal.Controllers
+{
+    [Filters.UserLoginFilter]
+    public class TravelRecordController : Controller
+    {
+        // GET: TravelRecord
+        public ActionResult Index()
+        {
+            return PartialView("~/Views/TravelRecord/_Index.cshtml");
+        }
+    }
+}
--- AdminPortal/AdminPortal/Scripts/script_travel.js
+++ AdminPortal/AdminPortal/Scripts/script_travel.js
@@ -1,5 +1,6 @@
 ﻿const travelGlobalFunc = {
     centerPanel: document.querySelector('.center-panel'),
+    rightPanel: document.querySelector('.right-panel'),
 };
 (function TravelNew() {
     let menuBtn;
@@ -17,6 +18,52 @@
         const travelContainer = parseDoc.querySelector('.container');
 
         travelGlobalFunc.centerPanel.appendChild(travelContainer);
+        AssignEventListener(travelContainer);
+    }
+    function AssignEventListener(travelContainer) {
+        travelContainer.querySelector('.jsAddTravelers').addEventListener('click', function () {
+
+        });
+        travelContainer.querySelector('.jsAddItenerary').addEventListener('click', function () { });
+        travelContainer.querySelector('.jsAddAccomodation').addEventListener('click', function () { });
+
+        //DropdownList();
+        //DropdownList();
+        //DropdownList();
+        //DropdownList();
+    }
+})();
+
+
+
+(function TravelRecord() {
+
+    let btn = document.querySelector('.jsClickRecordsTravel');
+
+    if (btn) {
+        btn.addEventListener('click', async function (e) {
+            let view = await fetchView(AppGlobal.baseUrl + 'TravelRecord/Index');
+
+            responseSuccessIndexRecord(view);
+        });
+    }
+
+    async function responseSuccessIndexRecord(view) {
+
+        travelGlobalFunc.centerPanel.innerHTML = '';
+        travelGlobalFunc.rightPanel.innerHTML = '';
+
+
+        let parser = new DOMParser().parseFromString(view, 'text/html').querySelector('.container');
+
+
+        travelGlobalFunc.centerPanel.appendChild(parser);
+
+        assignEventListenersRecord(parser);
+    }
+
+    function assignEventListenersRecord(doc) {
 
     }
+
 })();
